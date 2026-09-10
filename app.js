1ed039f7cb38bbad6620ff59529ecec6920e39c7
raraacademy <raraapproved@gmail.com>
2026-09-11 02:39:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ function setStatus(text, type = "") {
   if (!el) return;
 
   el.textContent = text;
-  el.className = type ? status ${type} : "status";
+
+  el.className = type
+    ? status ${type}
+    : "status";
 }
 
 
@@ -39,4 +42,4 @@ function showLogin() {
     login.classList.remove("hidden");
   }
 
-  if (app) …
+  …
